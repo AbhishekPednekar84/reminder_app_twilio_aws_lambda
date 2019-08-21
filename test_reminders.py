72625bfbd93159f83e6e987e3d21ab59984cb537
Abhishek Pednekar <abhi_ap@hotmail.com>
2019-08-21 15:57:51 +0530

    Added an images folder + changes to the test file

--- test_reminders.py
+++ test_reminders.py
@@ -2,7 +2,7 @@ import os
 
 
 def test_json():
-    """This method simply tests if the directory.json and reminders.json files are present in the project directory. If either file is missing, then the test fails.
+    """This method simply tests if the directory.json and reminder_events.json files are present in the project directory. If either file is missing, then the test fails.
     """
     assert os.path.isfile(".\\directory.json")
-    assert os.path.isfile(".\\reminders.json")
+    assert os.path.isfile(".\\reminder_events.json")
